101da40cf4861c748e76d622ed56753d943c9847
jc rangon <jc.rangon@gmail.com>
2023-02-02 22:47:42 +0100

    preparation TP complete

--- regions-france/src/app/app.module.ts
+++ regions-france/src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegionsComponent } from './pages/regions/regions.component';
+
+// locale:
+
+import * as fr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+import { UcfirstPipe } from './pipes/ucfirst.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegionsComponent,
+    UcfirstPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue:'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default)
+  }
+}
---
+++ regions-france/src/app/pipes/ucfirst.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+
+@Pipe({
+
+    name: 'ucfirst',
+
+})
+
+
+export class UcfirstPipe implements PipeTransform
+
+{
+
+    transform(value: string|null): string|null {
+
+        if(value && value.length > 0){
+
+            return value[0].toUpperCase() + value.substring(1);
+
+        } else {
+
+            return value;
+
+        }
+
+    }
+
+}
